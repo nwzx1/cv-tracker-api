17c71d130c589398855ef69fb46cc753c6823dc8
nawas charoliya <nawascharoliya1@gmail.com>
2024-09-14 13:11:49 +0530

    s

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import os
 from flask import Flask
-from src.routes.cv_get import routes
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-app.register_blueprint(routes, url_prefix="/cv")
 
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def index():
-    return "nawas"
+    return {
+        "name": "123"
+    }
 
 
 # ****************** main **********************
 if __name__ == "__main__":
     app.run(
-        debug=True,
+        # debug=True,
         host="0.0.0.0",
         port=int(os.environ.get("PORT", 3000))
     )
